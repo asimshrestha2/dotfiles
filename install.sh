59dbf6d486ec5ad5070f8532d7df97143d8ab1cb
AS <asims178@gmail.com>
2025-07-16 21:06:17 -0400

    update tmux and install script

---
+++ install.sh
@@ -0,0 +1,36 @@
+#!/usr/bin/bash
+
+error() { echo "${red}ERROR:${plain} $*"; exit 1; }
+available() { command -v $1 >/dev/null; }
+
+echo "Installing programs and dotfiles"
+
+. /etc/os-release
+
+OS_NAME=$ID
+
+SUDO=
+if [ "$(id -u)" -ne 0 ]; then
+	# Running as root, no need for sudo
+	if ! available sudo; then
+		error "This script requires superuser permissions. Please re-run as root."
+	fi
+
+	SUDO="sudo"
+fi
+
+case $OS_NAME in
+	arch)
+		$SUDO pacman -Syu tmux fastfetch steam flatpak gcc make git ripgrep fd unzip neovim
+	;;
+	debian|ubuntu) 
+		$SUDO add-apt-repository ppa:neovim-ppa/unstable -y
+		$SUDO apt update
+		$SUDO apt -y install tmux fastfetch make gcc ripgrep unzip git xclip neovim
+	;;
+	*) exit ;;
+esac
+
+# Nvim config
+git clone https://github.com/asimshrestha2/kickstart.nvim.git "${XDG_CONFIG_HOME:-$HOME/.config}"/nvim
+
