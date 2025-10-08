92088f49c20f24143ccf60617896215c191316a4
AS <asims178@gmail.com>
2025-10-08 12:53:11 -0400

    add swaync better

--- cp_config_to_git.sh
+++ cp_config_to_git.sh
@@ -4,11 +4,14 @@ list=(
 	"waybar"
 	"mako"
 	"sway"
+	"swaync"
 	"swaylock"
 	"rofi"
+	"xdg-desktop-portal"
+	"xdg-desktop-portal-wlr"
 	# "fastfetch"
 )
 
 for i in "${list[@]}"; do
-	rsync --recursive "/home/$USER/.config/$i" "./.config"
+	rsync --verbose --recursive "/home/$USER/.config/$i" "./.config"
 done
