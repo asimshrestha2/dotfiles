76904756ff9c0718f1a486b4325aa88b5a460172
AS <asims178@gmail.com>
2025-10-15 21:03:22 -0400

    fix: macro implementation

---
+++ bin/move-to-and-launch.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+original_id=$(niri msg --json focused-window | jq .id)
+req_id=$(niri msg --json windows | jq --arg app_id "$3" '.[] | select(.app_id == $app_id) | .id')
+
+echo "$req_id $3"
+
+if [[ ! -z "${req_id}" ]]; then
+	notify-send "'$3' already open"
+	exit 
+fi
+
+niri msg action focus-monitor "$1"
+echo "$2" | nohup bash &
+sleep 0.5
+niri msg action consume-or-expel-window-left
+niri msg action move-window-up
+
+niri msg action focus-window --id $original_id
