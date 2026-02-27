503ba400d3d69dce9031d1de8785c59d184e49be
AS <asims178@gmail.com>
2026-02-26 20:40:49 -0500

    add kde shortcuts

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -3,7 +3,7 @@ if status is-interactive
     zoxide init fish --cmd cd | source
 
     if not string match -q -- "*tmux*" $TERM
-        fastfetch --kitty-direct /home/asim/Documents/term/107472573_p0.png --logo-height 16 --logo-width 27 --logo-padding 2 --logo-padding-top 1
+        fastfetch -l /home/asim/Documents/term/ado.png --logo-height 16 --logo-padding 2 --logo-padding-top 1
     end
 
     # fish_config theme choose "Rosé Pine"
--- cp_config_to_git.sh
+++ cp_config_to_git.sh
@@ -19,3 +19,11 @@ list=(
 for i in "${list[@]}"; do
 	rsync --verbose --recursive "/home/$USER/.config/$i" "./.config"
 done
+
+list_files=(
+	"kglobalshortcutsrc"
+)
+
+for i in "${list_files[@]}"; do
+	rsync --verbose "/home/$USER/.config/$i" "./.config"
+done
